7b07c3ce22d3841fa9e13d39803a1e9c3e7bf730
Ashanti <shamarkey03@yahoo.com>
2026-08-13 20:42:44 -0500

    random closing

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,27 +1,25 @@
 let name = prompt("A name:");
 let adjective = prompt("A adjective:");
 let noun = prompt("A noun:");
-let place = prompt("A place:");
-let verb = prompt("An action:");
-let noun1 = prompt("A noun:");
+
+let closings = [
+	"You are a legend.",
+	"Stay golden.",
+	"Signs point to awesome.",
+	];
+
+function randomNumber(a, b) {
+	return Math.floor(Math.random() * (b - a + 1))+ a;
+}
 
 function makeCard() {
 	console.log("Dear " + name + ",");
-	console.log("You are as " + adjective + " as an " + noun + ".");
-	console.log("I hope " + place + " is everything you expected.");
-	console.log(verb + " my baby girl for me.");
-	console.log("Happy birthday from the generator.");
+	console.log("You are as " + adjective + " as a " + noun + ".");
+	let index = randomNumber(0, closings.length - 1);
+	console.log(closings[index]);
 }
 
-setInterval(makeCard,2000);
-
-function thankYou() {
-	console.log("I love you and thank you for all the " + noun1 + " we shared.");
-}	
-
-thankYou();
-thankYou();
-thankYou();
-thankYou();
-	
+makeCard();
+makeCard();
+makeCard();
 
