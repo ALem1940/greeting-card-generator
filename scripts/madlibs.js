92cf9a4ea6389a2b879aab4392caeded77988796
Ashanti <shamarkey03@yahoo.com>
2026-08-14 20:23:38 -0500

    fancy or silly

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,22 +1,24 @@
 let name = prompt("A name:");
 let adjective = prompt("A adjective:");
 let noun = prompt("A noun:");
+let style = prompt("Fancy or silly");
 
 let closings = [
 	"You are a legend.",
 	"Stay golden.",
 	"Signs point to awesome.",
-	];
+];
 
 function randomNumber(a, b) {
 	return Math.floor(Math.random() * (b - a + 1))+ a;
 }
 
 function makeCard() {
-	console.log("Dear " + name + ",");
-	console.log("You are as " + adjective + " as a " + noun + ".");
-	let index = randomNumber(0, closings.length - 1);
-	console.log(closings[index]);
+	if (style === "fancy") {
+		console.log("Dearest " + name + ",");
+	} else {
+		console.log("YO " + name + "!");
+	}
 }
 
 makeCard();
